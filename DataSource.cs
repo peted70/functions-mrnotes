813fee776dd373ef321af3d62283cc7543d04ee6
unknown <peted70@hotmail.com>
2018-08-29 22:31:32 +0100

    renamed function

---
+++ DataSource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MRNotes
+{
+    internal class DataSource
+    {
+        public static List<MRNote> Notes { get; set; } = new List<MRNote>();
+    }
+}
---
+++ MRNote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MRNotes
+{
+    internal class MRNote
+    {
+        public DateTime Created { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}

c5e3b4ab9a5527f2c6817ef00923f445b74df9fc
unknown <peted70@hotmail.com>
2018-08-29 22:54:16 +0100

    initialise static data

--- DataSource.cs
+++ DataSource.cs
@@ -6,6 +6,15 @@ namespace MRNotes
 {
     internal class DataSource
     {
-        public static List<MRNote> Notes { get; set; } = new List<MRNote>();
+        public static List<MRNote> Notes { get; set; } = new List<MRNote>()
+        {
+            new MRNote
+            {
+                Author = "Pete D",
+                Created = DateTime.UtcNow,
+                Title = "Note 1",
+                Description = "First note by Pete D"
+            }
+        };
     }
 }

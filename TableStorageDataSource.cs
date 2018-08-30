0c2d3b774ddabe8ec994648ea3ace904fe35f1bc
unknown <peted70@hotmail.com>
2018-08-30 11:44:57 +0100

    added table get notes

---
+++ TableStorageDataSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MRNotes
+{
+    class TableStorageDataSource : IDataSource
+    {
+        public void AddNote(MRNote note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MRNote DeleteNote(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MRNote> GetNotes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
